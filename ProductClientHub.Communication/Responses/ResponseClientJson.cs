146664d309186cc63b7d8f4cab7c6d114097b29e
Eduardo Ribeiro <eduardo.ribeiro.1593869@sga.pucminas.br>
2026-03-25 18:43:29 -0300

    Add project files.

---
+++ ProductClientHub.Communication/Responses/ResponseClientJson.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductClientHub.Communication.Responses
+{
+    public class ResponseClientJson
+    {
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
